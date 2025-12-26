5f2a3f495cb041795c65d2deaaf2347e63bb7f75
alonepranav <star.pranav111@gmail.com>
2025-12-26 16:00:24 +0530

    Learning branch's first commit

--- NewLearning.java
+++ NewLearning.java
@@ -1,5 +1,6 @@
 public class NewLearning{
 	public static void main(String[] args){
+		System.out.println("This is for learning Branch");
 		System.out.println("Hello, Pranav");
 	}
 }
--- newTestin.ts
+++ newTestin.ts
@@ -1,2 +1,3 @@
+console.log("This is the Learning Branch")
 console.log("Hello, Pranav")
 console.log("Hello, Pranav 2")
